7a6001742e0f8c7909dc059dc63876cdc2d95a3a
Pierrot795 <64477709+Pierrot795@users.noreply.github.com>
2022-01-03 17:42:51 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def page1():
         #first_name = results[0]['awards']['investigators'][0]['first_name']
         #last_name = results[0]['awards']['investigators'][0]['last_name']
         #return "The investigator is: "+first_name+' '+last_name+' , and his mail address is: '+email
-        return render_template('page1_results.html',results=results)
+        return render_template('page1_results.html',results=results,id=award_id,title=award_title)
     return render_template('page1.html')
 
 
@@ -44,7 +44,7 @@ def page2():
         query = dbmongo.query2(name,db)
         results = query[0]['awards']
         print(results)
-        return render_template('page2_results.html',results=results)
+        return render_template('page2_results.html',results=results,name=name)
     return render_template('page2.html')
 
 @app.route('/page4',methods=['GET','POST'])
@@ -56,7 +56,7 @@ def page4():
         query = dbmongo.query4(first_name,last_name,email_id,db)
         results = query
         print(results)
-        return render_template('page4_results.html',results=results)
+        return render_template('page4_results.html',results=results,first_name=first_name,last_name=last_name)
     return render_template('page4.html')
 
 @app.route('/page3',methods=['GET','POST'])
@@ -65,7 +65,7 @@ def page3():
         name = request.form.get("scientific domain")
         query = dbmongo.query3(name,db)
         results = query[0]['w_award']
-        return render_template('page3_results.html',results=results)
+        return render_template('page3_results.html',results=results,name=name)
     return render_template('page3.html')
 
 @app.route('/page5',methods=['GET','POST'])
@@ -83,7 +83,7 @@ def page7():
         query = dbmongo.query7(date1,date2,db)
         results = query
         results2 = query[0]['awards']
-        return render_template('page7_results.html',results=results,results2=results2)
+        return render_template('page7_results.html',results=results,results2=results2,date1=date1,date2=date2)
     return render_template('page7.html')
 
 @app.route('/page6',methods=['GET','POST'])
